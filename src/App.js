20b1febd8aab5d6e0191cbfd8c2e7ef1d5481ff3
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-08-15 22:46:59 -0400

    added some more routing logic

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 
@@ -5,6 +6,19 @@ export default function App() {
   return (
     <div id="app-root">
       <Navbar />
+      <nav>
+        <ul>
+          <li>
+            <a href={`/hello`}>Hello</a>
+          </li>
+          <li>
+            <a href={`/goodbye`}>goodbye</a>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <Outlet />
+      </div>
     </div>
   )
 }
