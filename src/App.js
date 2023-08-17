5210dfcb2e59550275bcf281d2ea7288a6cce8b1
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-08-16 21:58:13 -0400

    adjusted placement of router code

--- src/App.js
+++ src/App.js
@@ -3,12 +3,34 @@ import Banner from './components/Banner';
 import NavBar from './components/Navbar';
 import { Outlet } from 'react-router-dom';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from './components/ErrorPage';
+import Hello from './components/Hello';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Hello />,
+    errorElement: <ErrorPage />,
+    // children: [
+    //   {
+    //     path: "hello",
+    //     element: <Hello />,
+    //   },
+    // ],
+  },
+]);
+
 export default function App() {
   return (
     <div id="app-root">
       <Banner />
       <NavBar />
       <Outlet />
+      <RouterProvider router={router} />
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,32 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Hello from './components/Hello';
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import ErrorPage from './components/ErrorPage';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "hello",
-        element: <Hello />,
-      },
-    ],
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
